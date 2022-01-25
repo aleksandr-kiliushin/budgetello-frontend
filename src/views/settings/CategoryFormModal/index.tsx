637ab2fc6e4c2698b8e83bcc2ca0406e379bf609
a.kilyushin <a.kilyushin@rambler-co.ru>
2022-01-25 18:57:22 +0300

    n51: Add tests for category creation modal: open and close with Close button

--- src/views/settings/CategoryFormModal/index.tsx
+++ src/views/settings/CategoryFormModal/index.tsx
@@ -3,13 +3,16 @@ import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
+import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
+import FormLabel from '@mui/material/FormLabel'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import TextField from '@mui/material/TextField'
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 
+import RowGroup from '#components/RowGroup'
 import { createCategoryTc, updateCategoryTc } from '#models/finance'
 import { FinanceCategory, FinanceCategoryType } from '#types/finance'
 import { useAppDispatch } from '#utils/hooks'
@@ -40,18 +43,23 @@ const CategoryFormModal: FC<Props> = ({ category, categoryTypes, closeModal }) =
       <DialogTitle>{category ? 'Edit category' : 'Create category'}</DialogTitle>
       <form onSubmit={submitCategoryForm}>
         <DialogContent>
-          <TextField label="Name" {...register(FormField.Name, { required: true })} />
-          <RadioGroup>
-            {categoryTypes.map(({ name, id }) => (
-              <FormControlLabel
-                control={<Radio />}
-                key={id}
-                label={name}
-                value={id}
-                {...register(FormField.TypeId, { required: true })}
-              />
-            ))}
-          </RadioGroup>
+          <RowGroup>
+            <TextField label="Name" {...register(FormField.Name, { required: true })} />
+            <FormControl>
+              <FormLabel htmlFor={FormField.TypeId}>Type</FormLabel>
+              <RadioGroup id={FormField.TypeId} aria-labelledby={FormField.TypeId}>
+                {categoryTypes.map(({ name, id }) => (
+                  <FormControlLabel
+                    control={<Radio />}
+                    key={id}
+                    label={name}
+                    value={id}
+                    {...register(FormField.TypeId, { required: true })}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </RowGroup>
         </DialogContent>
         <DialogActions>
           <Button onClick={closeModal}>Cancel</Button>
--- src/views/settings/index.test.tsx
+++ src/views/settings/index.test.tsx
@@ -1,7 +1,8 @@
 /** @jest-environment jsdom */
-import { screen } from '@testing-library/react'
+import { act, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import { financeCategories } from '#mocks/constants/finance'
+import { financeCategories, financeCategoryTypes } from '#mocks/constants/finance'
 import render from '#mocks/render'
 import Settings from '#views/settings'
 
@@ -12,5 +13,78 @@ describe('Finance categories service.', () => {
     financeCategories.forEach(async ({ name }) => {
       expect(await screen.findByText(name)).toBeInTheDocument()
     })
+  })
+
+  test('Modal for new category modal opens correctly.', async () => {
+    render(<Settings />)
+
+    const openModalButton = screen.getByRole('button', { name: '+New' })
+    expect(openModalButton).toBeInTheDocument()
+
+    act(() => {
+      userEvent.click(openModalButton)
+    })
+
+    const modalHeader = screen.getByRole('heading', { name: 'Create category' })
+    expect(modalHeader).toBeInTheDocument()
+
+    const nameInput = screen.getByLabelText('Name')
+    expect(nameInput).toBeInTheDocument()
+
+    financeCategoryTypes.forEach(async ({ name }) => {
+      const radioButton = await screen.findByLabelText(name)
+      expect(radioButton).toBeInTheDocument()
+    })
+
+    const closeModalButton = screen.getByRole('button', { name: 'Cancel' })
+    expect(closeModalButton).toBeInTheDocument()
+
+    const submitFormButton = screen.getByRole('button', { name: 'Submit' })
+    expect(submitFormButton).toBeInTheDocument()
+  })
+
+  test('Modal for new category creating closes correctly using Cancel button.', async () => {
+    render(<Settings />)
+
+    const openModalButton = screen.getByRole('button', { name: '+New' })
+    expect(openModalButton).toBeInTheDocument()
+
+    act(() => {
+      userEvent.click(openModalButton)
+    })
+
+    const modalHeader = screen.getByRole('heading', { name: 'Create category' })
+    expect(modalHeader).toBeInTheDocument()
+
+    const closeModalButton = screen.getByRole('button', { name: 'Cancel' })
+    act(() => {
+      userEvent.click(closeModalButton)
+    })
+    expect(modalHeader).not.toBeInTheDocument()
+  })
+
+  test('Modal for new category creating closes correctly using click on its backdrop.', async () => {
+    render(<Settings />)
+
+    const openModalButton = screen.getByRole('button', { name: '+New' })
+    expect(openModalButton).toBeInTheDocument()
+
+    act(() => {
+      userEvent.click(openModalButton)
+    })
+
+    const modalHeader = screen.getByRole('heading', { name: 'Create category' })
+    expect(modalHeader).toBeInTheDocument()
+
+    // ToDo: Add tests for closing on backdrop click.
+    // const modalBackdrop = await screen.findByRole('presentation')
+    // expect(modalBackdrop).toBeInTheDocument()
+    // act(() => {
+    //   // if (!modalBackdrop) {
+    //   //   throw new Error('Modal backdrop was not found.')
+    //   // }
+    //   userEvent.click(modalBackdrop)
+    // })
+    // expect(modalHeader).not.toBeInTheDocument()
   })
 })
