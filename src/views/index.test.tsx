883e0c497ab229a0cb179bb84bf7a982f6b9549b
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-11 19:38:17 +0400

    n52: feat: Add wait util

--- src/views/index.test.tsx
+++ src/views/index.test.tsx
@@ -1,5 +1,7 @@
 import { waitFor } from "@testing-library/react"
 
+// import { wait } from "@testing-library/user-event/dist/utils"
+// import { login } from "#models/user"
 import { render } from "#utils/testing/render"
 
 import App from "./index"
@@ -9,4 +11,11 @@ describe("<App />", () => {
     const { history } = render(<App />, { initialUrl: "/settings" })
     await waitFor(() => expect(history.location.pathname).toEqual("/auth"))
   })
+
+  // test.skip("If user is authorized they is NOT redirected to /auth.", async () => {
+  //   const { history, store } = render(<App />, { initialUrl: "/settings" })
+  //   store.dispatch(login({ username: "john-doe", password: "john-doe-password" }))
+  //   await wait(1500)
+  //   expect(history.location.pathname).toEqual("/settings")
+  // })
 })
