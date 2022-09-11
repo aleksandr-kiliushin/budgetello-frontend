7286041937150a96d7e7ad4599f868c5d6f89112
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-11 15:14:28 +0400

    n52: chore: Use sync exec in test hooks

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,21 +1,16 @@
 import "@testing-library/jest-dom"
-import { exec } from "child_process"
-import util from "util"
+import { execSync } from "child_process"
 // TODO: Uninstall and use Node.js fetch instead.
 import "whatwg-fetch"
 
-const execAsync = util.promisify(exec)
-
-beforeEach(async () => {
-  await execAsync(
-    'echo "bash /var/app/restore-db-from-testing-template.sh" | docker exec -i personal-app-database bash;'
-  )
+beforeEach(() => {
+  execSync('echo "bash /var/app/restore-db-from-testing-template.sh" | docker exec -i personal-app-database bash;')
 })
 
 afterEach(() => {
   localStorage.removeItem("authToken")
 })
 
-afterAll(async () => {
-  await execAsync('echo "bash /var/app/restore-db-from-dev-template.sh" | docker exec -i personal-app-database bash;')
+afterAll(() => {
+  execSync('echo "bash /var/app/restore-db-from-dev-template.sh" | docker exec -i personal-app-database bash;')
 })
