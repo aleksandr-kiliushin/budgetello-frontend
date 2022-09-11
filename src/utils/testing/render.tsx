e05344d8e6b8d64d63542ffef542353e3d2d0de6
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-11 19:40:42 +0400

    n52: refactor: render function

--- src/utils/testing/render.tsx
+++ src/utils/testing/render.tsx
@@ -1,24 +1,19 @@
 import { render as rtlRender } from "@testing-library/react"
-import { BrowserHistory, createBrowserHistory } from "history"
+import { createBrowserHistory } from "history"
 import React from "react"
 import { Provider } from "react-redux"
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom"
 
-import { IStore, initializeStore } from "#models/store"
+import { initializeStore } from "#models/store"
 
 import { IRender } from "./types"
 
-let history: BrowserHistory
-let store: IStore
-beforeEach(async () => {
-  history = createBrowserHistory()
-  store = initializeStore()
-})
-
 export const render: IRender = (component, options) => {
-  const AllTheProviders: React.FC = ({ children }) => {
-    history.push(options?.initialUrl ?? "/")
+  const store = initializeStore()
+  const history = createBrowserHistory()
+  history.push(options?.initialUrl ?? "/")
 
+  const AllTheProviders: React.FC = ({ children }) => {
     return (
       <HistoryRouter history={history}>
         <Provider store={store}>{children}</Provider>
