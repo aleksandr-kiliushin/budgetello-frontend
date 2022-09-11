d837c3aee670bd1e50642bb197c17c046d3f4aeb
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-11 14:32:32 +0400

    n52: refactor: tests

--- src/views/settings/index.test.tsx
+++ src/views/settings/index.test.tsx
@@ -61,17 +61,14 @@ describe("Finance categories service", () => {
 
     expect(await screen.findByText("salary")).toBeInTheDocument()
     await waitFor(() => userEvent.click(screen.getByTestId("salary-income-category-edit-button")))
-
     expect(screen.getByRole("dialog")).toBeInTheDocument()
     expect(screen.getByLabelText("Name")).toHaveValue("salary")
     expect(await screen.findByLabelText("income")).toBeChecked()
-
     await waitFor(() => userEvent.clear(screen.getByLabelText("Name")))
     await waitFor(() => userEvent.type(screen.getByLabelText("Name"), "casino"))
     await waitFor(() => userEvent.click(screen.getByLabelText("expense")))
     userEvent.click(screen.getByText("Submit"))
     await waitForElementToBeRemoved(() => screen.getByRole("dialog"))
-
     expect(await screen.findByText("casino")).toBeInTheDocument()
     expect(screen.getByTestId("casino-expense-category-edit-button")).toBeInTheDocument()
     expect(screen.queryByText("salary")).not.toBeInTheDocument()
