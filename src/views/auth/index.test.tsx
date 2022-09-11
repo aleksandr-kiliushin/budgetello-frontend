5000678c4d62aaf606820ecaa0c45cde15e50fad
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-09-11 14:51:39 +0400

    n52: tests: Refactor Login works correctly.

--- src/views/auth/index.test.tsx
+++ src/views/auth/index.test.tsx
@@ -1,5 +1,5 @@
 /** @jest-environment jsdom */
-import { screen, waitFor } from "@testing-library/react"
+import { screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 import { login } from "#models/user"
@@ -7,41 +7,22 @@ import { render } from "#utils/testing/render"
 import Auth from "#views/auth"
 
 describe("Auth service.", () => {
-  test.skip("Login works correctly.", async () => {
+  test("Login works correctly.", async () => {
     render(<Auth />)
+
     expect(localStorage.authToken).toBeUndefined()
-
-    const welcomeHeading = screen.getByRole("heading", { name: "Welcome" })
-
-    const logInButton = screen.getByRole("button", { name: "Log in" })
-    expect(logInButton).toBeDisabled()
-
-    const usernameInput = screen.getByLabelText("Username")
-    const passwordInput = screen.getByLabelText("Password")
-
-    userEvent.type(usernameInput, "john-doe")
-    userEvent.type(passwordInput, "john-doe-password")
-
-    await waitFor(() => {
-      expect(logInButton).toBeEnabled()
-    })
-
-    userEvent.click(logInButton)
-
-    await waitFor(() => {
-      expect(welcomeHeading).not.toBeInTheDocument()
-    })
-
+    expect(screen.getByText("Welcome")).toBeInTheDocument()
+    expect(screen.getByText("Log in")).toBeDisabled()
+    await waitFor(() => userEvent.type(screen.getByLabelText("Username"), "john-doe"))
+    await waitFor(() => userEvent.type(screen.getByLabelText("Password"), "john-doe-password"))
+    expect(screen.getByText("Log in")).toBeEnabled()
+    userEvent.click(screen.getByText("Log in"))
+    await waitForElementToBeRemoved(() => screen.getByText("Welcome"))
     expect(localStorage.authToken).toEqual(expect.stringMatching(".+"))
-    expect(logInButton).not.toBeInTheDocument()
-
-    expect(screen.getByRole("button", { name: "Log out" })).toBeInTheDocument()
-
+    expect(screen.queryByText("Log in")).not.toBeInTheDocument()
+    expect(screen.getByText("Log out")).toBeInTheDocument()
     await waitFor(() => {
-      const youAreLoggedInParagraph = screen.getByText(
-        (content, node) => node?.textContent === `You are logged in as john-doe.`
-      )
-      expect(youAreLoggedInParagraph).toBeInTheDocument()
+      expect(screen.getByText(/You are logged in/)).toHaveTextContent("You are logged in as john-doe.")
     })
   })
 
