a46e9269a1ac9124f8b1a6eb8ebdbabe1537c38d
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-03 00:37:37 +0400

    n60: test: Update e2e tets

--- cypress/e2e/board-records.cy.ts
+++ cypress/e2e/board-records.cy.ts
@@ -2,20 +2,8 @@ describe("records", () => {
   it("are loaded correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records")
+    cy.visit("/boards/1/records")
     cy.url().should("include", "/records?isTrash=false")
-
-    cy.get("td").contains("230").should("be.visible")
-    cy.get("td").contains("gifts").should("be.visible")
-    cy.get("td").contains("22-08-03").should("be.visible")
-
-    cy.get("td").contains("10").should("be.visible")
-    cy.get("td").contains("gifts").should("be.visible")
-    cy.get("td").contains("22-08-02").should("be.visible")
-
-    cy.get("td").contains("30").should("be.visible")
-    cy.get("td").contains("gifts").should("be.visible")
-    cy.get("td").contains("22-08-02").should("be.visible")
 
     cy.get("td").contains("25").should("be.visible")
     cy.get("td").contains("education").should("be.visible")
@@ -36,26 +24,26 @@ describe("records", () => {
   it("is created correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records")
+    cy.visit("/boards/1/records")
     cy.get("button").contains("+New").click()
 
     cy.get("input[name='amount']").type("0")
     cy.get("#mui-component-select-categoryId").click()
-    cy.get("[role='option']").contains("salary").click()
+    cy.get("[role='option']").contains("education").click()
     cy.get("p").contains("amount must be a positive number").should("be.visible")
     cy.get("input[name='amount']").clear().type("2000")
     // TODO: Enter a date.
     cy.get("button").contains("Submit").click()
 
     cy.get("td").contains("2000").should("be.visible")
-    cy.get("td").contains("salary").should("be.visible")
+    cy.get("td").contains("education").should("be.visible")
     // TODO: Check the date.
   })
 
   it("is edited correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records")
+    cy.visit("/boards/1/records")
     cy.get("#2022-08-01-expense-education-25-edit-button").click()
 
     cy.get("input[name='amount']").clear().type("6666")
@@ -71,7 +59,7 @@ describe("records", () => {
   it("is moved to trash correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records")
+    cy.visit("/boards/1/records")
     cy.get("#2022-08-01-expense-education-25-delete-button").click()
 
     cy.get("input[name='isTrash']").click()
@@ -84,7 +72,7 @@ describe("records", () => {
   it("is restored correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records?isTrash=true")
+    cy.visit("/boards/1/records?isTrash=true")
     cy.get("#2022-08-01-expense-clothes-100-restore-button").click()
 
     cy.get("input[name='isTrash']").click()
@@ -97,7 +85,7 @@ describe("records", () => {
   it("is deleted from trash correctly", () => {
     cy.authorize("john-doe")
 
-    cy.visit("/records?isTrash=true")
+    cy.visit("/boards/1/records?isTrash=true")
     cy.get("#2022-08-01-expense-clothes-100-delete-button").click()
 
     cy.get("td").contains("100").should("not.exist")
