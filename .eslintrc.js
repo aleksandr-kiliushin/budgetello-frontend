60d0611e744a15caec4e4ff6c3044a6bec79167d
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-12 14:34:51 +0400

    n65: feat: Modal from useModal opens and closes

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     ],
     "no-tabs": 1,
     "react/jsx-sort-props": ["warn", { ignoreCase: true }],
+    "@typescript-eslint/no-empty-interface": 0,
     "@typescript-eslint/no-extra-semi": 1,
     "@typescript-eslint/no-unused-vars": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
--- cypress/component/Modal.cy.tsx
+++ cypress/component/Modal.cy.tsx
@@ -3,13 +3,24 @@ import React from "react"
 import { useModal } from "#components/useModal"
 
 const TestModalComponent: React.FC = () => {
-  const [Modal] = useModal()
-  return <Modal />
+  const [MyModal, openMyModal, closeMyModal] = useModal({ isOpenInitially: false })
+
+  return (
+    <>
+      <button onClick={openMyModal}>open</button>
+      <button onClick={closeMyModal}>close</button>
+      <MyModal>MyModal</MyModal>
+    </>
+  )
 }
 
-describe("Modal.cy.ts", () => {
-  it("playground", () => {
+describe("useModal", () => {
+  it("open-close works", () => {
     cy.mount(<TestModalComponent />)
-    cy.contains("Modal").should("be.visible")
+    cy.contains("MyModal").should("not.exist")
+    cy.contains("open").click()
+    cy.contains("MyModal").should("be.visible")
+    cy.contains("close").click()
+    cy.contains("MyModal").should("not.exist")
   })
 })
