4cb7b9d787f8c8f495c6b3a2db4179f172b450af
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-23 18:59:41 +0400

    n70: test: Authorization page JEST -> CYPRESS

---
+++ cypress/e2e/authorization.cy.ts
@@ -0,0 +1,53 @@
+import { testUsers } from "#utils/testing/test-users"
+
+describe("Authorization", () => {
+  it("logs in successfully", () => {
+    cy.visit("/auth")
+
+    expect(localStorage.authToken).to.be.equal(undefined)
+    cy.contains("Welcome").should("be.visible")
+    cy.contains("Log in").should("be.disabled")
+    cy.get('input[name="username"]').type("john-doe")
+    cy.get('input[name="password"]').type("john-doe-password")
+    cy.contains("Log in").should("be.enabled").click()
+    cy.contains("Welcome").should("not.exist")
+    expect(localStorage.authToken).to.match(/.+/)
+    cy.contains("Log in").should("not.exist")
+    cy.contains("Log out").should("be.visible")
+    cy.contains("You are logged in as john-doe.").should("be.visible")
+  })
+
+  it("case: user enters unexisting username", () => {
+    cy.visit("/auth")
+
+    cy.get('input[name="username"]').type("john-doe-WITH-A-TYPO")
+    cy.get('input[name="password"]').type("john-doe-password")
+    cy.contains("Log in").click()
+    cy.contains("User not found.").should("be.visible")
+    cy.get('input[name="username"]').clear().type("john-doe")
+    cy.contains("Log in").click()
+    cy.contains("You are logged in as john-doe.").should("be.visible")
+  })
+
+  it("case: user enters invalid password", () => {
+    cy.visit("/auth")
+
+    cy.get('input[name="username"]').type("john-doe")
+    cy.get('input[name="password"]').type("john-doe-password-WITH-A-TYPO")
+    cy.contains("Log in").click()
+    cy.contains("Invalid password.").should("be.visible")
+    cy.get('input[name="password"]').clear().type("john-doe-password")
+    cy.contains("Log in").click()
+    cy.contains("You are logged in as john-doe.").should("be.visible")
+  })
+
+  it("logs out successfully", () => {
+    cy.authorize(testUsers.johnDoe.id)
+    cy.visit("/auth")
+
+    expect(localStorage.authToken).to.match(/.+/)
+    cy.contains("Log out").click()
+    cy.contains("Log in").should("be.visible")
+    expect(localStorage.authToken).to.be.equal(undefined)
+  })
+})
