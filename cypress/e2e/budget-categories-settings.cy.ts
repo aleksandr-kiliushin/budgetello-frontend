a90e751bf74106bd03826092575cab449c496fe3
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-23 18:02:23 +0400

    n70: test: case: user tries to create a category that already exists and then fixes input values JEST -> CYPRESS

--- cypress/e2e/budget-categories-settings.cy.ts
+++ cypress/e2e/budget-categories-settings.cy.ts
@@ -32,6 +32,35 @@ describe("Budget categories settings", () => {
     cy.contains("travel").should("be.visible")
   })
 
+  it.only("case: user tries to create a category that already exists and then fixes input values", () => {
+    cy.authorize(testUsers.johnDoe.id)
+    cy.visit("/boards/1/settings")
+
+    cy.contains("+New").click()
+    cy.get('input[name="name"]').type("education")
+    cy.get('input[name="typeId"][value="1"]').click() // Select "Expense" category type.
+    cy.contains("Submit").click()
+    cy.get("body")
+      .find("p")
+      .filter((index, node) => node.innerText === '"education" expense category already exists in this board.')
+      .should("have.length", 2)
+    cy.contains("Submit").should("be.disabled")
+    cy.get('input[name="name"]').clear()
+    cy.get("body")
+      .find("p")
+      .filter((index, node) => node.innerText === '"education" expense category already exists in this board.')
+      .should("have.length", 1)
+    cy.get('input[name="name"]').type("teaching")
+    cy.get('input[name="typeId"][value="2"]').click() // Select "Income" category type.
+    cy.get("body")
+      .find("p")
+      .filter((index, node) => node.innerText === '"education" expense category already exists in this board.')
+      .should("have.length", 0)
+    cy.contains("Submit").click()
+    cy.get('[role="dialog"]').should("not.exist")
+    cy.contains("teaching").should("be.visible")
+  })
+
   it("is deleted correctly", () => {
     cy.authorize(testUsers.johnDoe.id)
     cy.visit("/boards/1/settings")
--- src/views/boards/BoardSettings/index.test.tsx
+++ src/views/boards/BoardSettings/index.test.tsx
@@ -6,29 +6,6 @@ import { testUsers } from "#utils/testing/test-users"
 import { App } from "#views/index"
 
 describe("Budget categories", () => {
-  test("case: user tries to create a category that already exists and then fixes input values.", async () => {
-    await render(<App />, { iAm: testUsers.johnDoe.id, initialUrl: "/boards/1/settings" })
-
-    userEvent.click(await screen.findByText("+New"))
-    await waitFor(() => userEvent.type(screen.getByLabelText("Name"), "education"))
-    await waitFor(() => userEvent.click(screen.getByLabelText("expense")))
-    userEvent.click(screen.getByText("Submit"))
-    expect(await screen.findAllByText('"education" expense category already exists in this board.')).toHaveLength(2)
-    expect(screen.getByText("Submit")).toBeDisabled()
-    await waitFor(() => userEvent.clear(screen.getByLabelText("Name")))
-    await waitFor(() => {
-      expect(screen.getAllByText('"education" expense category already exists in this board.')).toHaveLength(1)
-    })
-    await waitFor(() => userEvent.type(screen.getByLabelText("Name"), "teaching"))
-    await waitFor(() => userEvent.click(screen.getByLabelText("income")))
-    await waitFor(() => {
-      expect(screen.queryAllByText('"education" expense category already exists in this board.')).toHaveLength(0)
-    })
-    await waitFor(() => userEvent.click(screen.getByText("Submit")))
-    await waitForElementToBeRemoved(() => screen.getByRole("dialog"))
-    expect(await screen.findByRole("cell", { name: "teaching" })).toBeInTheDocument()
-  })
-
   test("A category is edited correctly.", async () => {
     await render(<App />, { iAm: testUsers.jessicaStark.id, initialUrl: "/boards/2/settings" })
 
