2711418c4a9dc087800f1e2f9ff287b8d1eb91bf
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-23 18:40:24 +0400

    n70: test: redirecting JEST -> CYPRESS

---
+++ cypress/e2e/redirect-unauthorized-users.cy.ts
@@ -0,0 +1,15 @@
+import { testUsers } from "#utils/testing/test-users"
+
+describe("redirect unauthorized users", () => {
+  it("unauthorized users are redirected to /auth.", () => {
+    cy.visit("/boards")
+    cy.url().should("include", "/auth")
+  })
+
+  it("authorized users are NOT redirected to /auth.", () => {
+    cy.authorize(testUsers.johnDoe.id)
+    cy.visit("/boards")
+    cy.wait(500)
+    cy.url().should("include", "/boards")
+  })
+})
