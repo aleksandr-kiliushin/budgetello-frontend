a014a81d845a2881d69eef329e587232ae8a99db
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-24 14:05:47 +0400

    n73: test: Closing dialog from another component

--- cypress/component/Dialog.cy.tsx
+++ cypress/component/Dialog.cy.tsx
@@ -2,8 +2,20 @@ import React from "react"
 
 import { useDialog } from "#components/useDialog"
 
+const MySampleDialogBody = () => {
+  const [, , closeMyDialog] = useDialog({ id: "sample-dialog" })
+
+  return (
+    <div>
+      <p>My dialog body.</p>
+      <p>You can close the dialog from another component.</p>
+      <button onClick={closeMyDialog}>Closing button in dialog body</button>
+    </div>
+  )
+}
+
 const SampleComponentWithDialog: React.FC = () => {
-  const [MyDialog, openMyDialog, closeMyDialog] = useDialog({ id: "sample-dialog", isOpenInitially: false })
+  const [MyDialog, openMyDialog, closeMyDialog] = useDialog({ id: "sample-dialog" })
 
   return (
     <>
@@ -12,9 +24,11 @@ const SampleComponentWithDialog: React.FC = () => {
         <MyDialog.Header>
           <h2 style={{ margin: 0 }}>My dialog heading</h2>
         </MyDialog.Header>
-        <MyDialog.Body>My dialog body.</MyDialog.Body>
+        <MyDialog.Body>
+          <MySampleDialogBody />
+        </MyDialog.Body>
         <MyDialog.Footer>
-          <button onClick={closeMyDialog}>close</button>
+          <button onClick={closeMyDialog}>Close</button>
         </MyDialog.Footer>
       </MyDialog>
     </>
@@ -32,9 +46,11 @@ describe("useDialog", () => {
     cy.contains("My dialog heading").should("be.visible")
     cy.contains("My dialog body.").should("be.visible")
     cy.contains("close").should("be.visible")
-    // cy.contains("close").click()
-    // cy.contains("My dialog heading").should("not.exist")
-    // cy.contains("My dialog body.").should("not.exist")
-    // cy.contains("close").should("not.exist")
+    cy.contains("Closing button in dialog body").click()
+    cy.contains("My dialog heading").should("not.exist")
+    cy.contains("My dialog body.").should("not.exist")
+    cy.contains("close").should("not.exist")
+
+    cy.contains("open").click()
   })
 })
