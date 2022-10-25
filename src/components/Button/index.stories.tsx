d347a9519a834745d9ed473f9fda5f37a6b2f613
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-10-25 18:51:45 +0400

    n75: feat: Style button and document them

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,6 +1,7 @@
-import { AddReactionOutlined as AddReactionOutlinedIcon } from "@mui/icons-material"
-import { Button, IconButton } from "@mui/material"
-import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { ArrowCircleDownOutlined as ArrowCircleDownOutlinedIcon } from "@mui/icons-material"
+import { Button, styled } from "@mui/material"
+import { ComponentMeta } from "@storybook/react"
+import React from "react"
 
 // eslint-disable-next-line no-restricted-syntax
 export default {
@@ -8,56 +9,105 @@ export default {
   title: "Button",
 } as ComponentMeta<typeof Button>
 
-const ButtonTemplate: ComponentStory<typeof Button> = (args) => {
-  return <Button {...args}>Click me</Button>
-}
+const Grid = styled("div")({
+  display: "grid",
+  gridAutoFlow: "column",
+  columnGap: "12px",
+  border: "2px solid black",
+})
 
-export const ButtonPrimarySmallContained = ButtonTemplate.bind({})
-ButtonPrimarySmallContained.args = {
-  color: "primary",
-  size: "small",
-  variant: "contained",
-}
+const Group = styled("div")({
+  display: "grid",
+  rowGap: "12px",
+})
 
-export const ButtonSecondaryMediumContained = ButtonTemplate.bind({})
-ButtonSecondaryMediumContained.args = {
-  color: "secondary",
-  size: "medium",
-  variant: "outlined",
-}
-
-export const ButtonErrorMediumText = ButtonTemplate.bind({})
-ButtonErrorMediumText.args = {
-  color: "error",
-  size: "medium",
-  variant: "text",
-}
-
-const IconButtonTemplate: ComponentStory<typeof Button> = (args) => {
+export const Buttons: React.FC = () => {
   return (
-    <IconButton {...args}>
-      <AddReactionOutlinedIcon />
-    </IconButton>
+    <Grid>
+      <Group>
+        <Button color="primary" size="small" variant="contained">
+          Click me
+        </Button>
+        <Button color="primary" size="medium" variant="contained">
+          Click me
+        </Button>
+        <Button color="primary" size="small" variant="outlined">
+          Click me
+        </Button>
+        <Button color="primary" size="medium" variant="outlined">
+          Click me
+        </Button>
+        <Button color="primary" size="small" variant="text">
+          Click me
+        </Button>
+        <Button color="primary" size="medium" variant="text">
+          Click me
+        </Button>
+      </Group>
+      <Group>
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained">
+          Click me
+        </Button>
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained">
+          Click me
+        </Button>
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined">
+          Click me
+        </Button>
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined">
+          Click me
+        </Button>
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text">
+          Click me
+        </Button>
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text">
+          Click me
+        </Button>
+      </Group>
+      <Group>
+        <Button color="error" size="small" variant="contained">
+          Click me
+        </Button>
+        <Button color="error" size="medium" variant="contained">
+          Click me
+        </Button>
+        <Button color="error" size="small" variant="outlined">
+          Click me
+        </Button>
+        <Button color="error" size="medium" variant="outlined">
+          Click me
+        </Button>
+        <Button color="error" size="small" variant="text">
+          Click me
+        </Button>
+        <Button color="error" size="medium" variant="text">
+          Click me
+        </Button>
+      </Group>
+      <Group>
+        <Button color="primary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="primary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="primary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="primary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="primary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+        <Button color="primary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+      </Group>
+      <Group>
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="secondary" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+        <Button color="secondary" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+      </Group>
+      <Group>
+        <Button color="error" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="error" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="contained" />
+        <Button color="error" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="error" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="outlined" />
+        <Button color="error" size="small" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+        <Button color="error" size="medium" startIcon={<ArrowCircleDownOutlinedIcon />} variant="text" />
+      </Group>
+    </Grid>
   )
-}
-
-export const IconButtonPrimarySmallContained = IconButtonTemplate.bind({})
-IconButtonPrimarySmallContained.args = {
-  color: "primary",
-  size: "small",
-  variant: "contained",
-}
-
-export const IconButtonSecondaryMediumOutlined = IconButtonTemplate.bind({})
-IconButtonSecondaryMediumOutlined.args = {
-  color: "secondary",
-  size: "medium",
-  variant: "outlined",
-}
-
-export const IconButtonErrorMediumText = IconButtonTemplate.bind({})
-IconButtonErrorMediumText.args = {
-  color: "error",
-  size: "medium",
-  variant: "text",
 }
--- src/styles/theme/components/MuiButton.ts
+++ src/styles/theme/components/MuiButton.ts
@@ -1,7 +1,30 @@
 import { Components, Theme } from "@mui/material"
 
 export const MuiButton: NonNullable<Components<Theme>["MuiButton"]> = {
+  defaultProps: {
+    disableRipple: true,
+  },
   styleOverrides: {
-    root: {},
+    root: {
+      minWidth: "unset",
+      width: "fit-content",
+      borderRadius: 0,
+      "&, &:hover": {
+        boxShadow: "none",
+      },
+      "&.MuiButton-sizeSmall": {
+        columnGap: "4px",
+        height: "32px",
+        padding: "0 8px",
+      },
+      "&.MuiButton-sizeMedium": {
+        columnGap: "8px",
+        height: "48px",
+        padding: "0 14px",
+      },
+    },
+    startIcon: {
+      margin: 0,
+    },
   },
 }
