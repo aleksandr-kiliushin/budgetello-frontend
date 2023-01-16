139dd46f34109bcf31adc6f37d26fef5750115dd
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-01-16 16:13:00 +0400

    Increase MuiSvgIcon size

---
+++ src/styles/theme/components/MuiSvgIcon.ts
@@ -0,0 +1,10 @@
+import { Components, Theme } from "@mui/material"
+
+export const MuiSvgIcon: NonNullable<Components<Theme>["MuiSvgIcon"]> = {
+  styleOverrides: {
+    root: {
+      height: "2rem",
+      width: "2rem",
+    },
+  },
+}
--- src/styles/theme/components/index.ts
+++ src/styles/theme/components/index.ts
@@ -3,11 +3,13 @@ import { Components, Theme } from "@mui/material"
 import { MuiButton } from "./MuiButton"
 import { MuiFormControl } from "./MuiFormControl"
 import { MuiOutlinedInput } from "./MuiOutlinedInput"
+import { MuiSvgIcon } from "./MuiSvgIcon"
 import { MuiTableCell } from "./MuiTableCell"
 
 export const components: NonNullable<Components<Theme>> = {
   MuiButton,
   MuiFormControl,
-  MuiTableCell,
   MuiOutlinedInput,
+  MuiTableCell,
+  MuiSvgIcon,
 }
