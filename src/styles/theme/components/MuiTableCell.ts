6f452bf9b4a7a2aa0f48873dc658c5f16608b4ff
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-01-19 11:44:20 +0400

    fix: Decrease tables header font weight

--- src/styles/theme/components/MuiTableCell.ts
+++ src/styles/theme/components/MuiTableCell.ts
@@ -2,9 +2,6 @@ import { Components, Theme } from "@mui/material"
 
 export const MuiTableCell: NonNullable<Components<Theme>["MuiTableCell"]> = {
   styleOverrides: {
-    head: {
-      fontWeight: "bold",
-    },
     sizeMedium: {
       height: "40px",
       padding: "4px",
