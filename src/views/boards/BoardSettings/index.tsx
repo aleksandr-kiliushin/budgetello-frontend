5c213a34c115a72fb20e240dac90d9dc36f52f44
Aleksandr <aleksandr.kiliushin@gmail.com>
2023-01-22 17:22:09 +0400

    n88: feat: Redirect non-admins from board settings

--- src/views/boards/BoardSettings/index.tsx
+++ src/views/boards/BoardSettings/index.tsx
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { FC } from "react"
+import { Navigate, useParams } from "react-router-dom"
+
+import { useGetBoardQuery } from "#api/boards"
+import { useGetUserQuery } from "#api/users"
 
 import { BudgetCategoriesSettings } from "./BudgetCategoriesSettings"
 import { GeneralSettings } from "./GeneralSettings"
 import { MembersSettings } from "./MembersSettings"
 
-export const BoardSettings: React.FC = () => {
+export const BoardSettings: FC = () => {
+  const params = useParams<{ boardId: string }>()
+
+  const getAuthorizedUserResult = useGetUserQuery({ variables: { id: 0 } })
+  const getBoardResult = useGetBoardQuery({ variables: { id: Number(params.boardId) } })
+
+  if (getAuthorizedUserResult.data === undefined) return null
+  if (getBoardResult.data === undefined) return null
+
+  const authorizedUser = getAuthorizedUserResult.data.user
+  const boardAdmins = getBoardResult.data.board.admins
+
+  if (boardAdmins.every((admin) => admin.id !== authorizedUser.id)) {
+    return <Navigate replace to={`/boards/${params.boardId}/records`} />
+  }
+
   return (
     <>
       <GeneralSettings />
